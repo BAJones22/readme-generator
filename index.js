9f48e27966159166908612e75976ae7202503c40
Brooke Adlesperger-Jones <badles@live.com>
2023-05-15 22:01:40 -0500

    added questions

---
+++ index.js
@@ -0,0 +1,15 @@
+const { questions } = require("./questions");
+const inquirer = require("inquirer");
+
+function askQuestions() {
+    inquirer.prompt(questions).then(function (answers) {
+        console.log(answers);
+    });
+}
+
+function main() {
+    console.log(questions);
+    askQuestions();
+}
+
+main();
---
+++ questions.js
@@ -0,0 +1,50 @@
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "projectName",
+  },
+  {
+    type: "input",
+    message: "Please give a description of your project:",
+    name: "projectDescription",
+  },
+  {
+    type: "input",
+    message: "Please provide any instructions required for installation of application:",
+    name: "projectInstallation",
+  },
+  {
+    type: "input",
+    message: "Please provide information regarding the usage of your application:",
+    name: "projectUsage",
+  },
+  {
+    type: "input",
+    message: "Please detail any guidelines you wish to provide for contributions to your project:",
+    name: "projectContribute",
+  },
+  {
+    type: "input",
+    message: "Please provide instructions on how to run all necessary tests:",
+    name: "projectTests",
+  },
+  {
+    type: "checkbox",
+    message: "Please select the type of license your project will contain:",
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    name: "projectLicense",
+  },
+  {
+    type: "input",
+    message: "Please provide your GitHub user name:",
+    name: "userGithHub",
+  },
+  {
+    type: "input",
+    message: "Please provide your email address:",
+    name: "userEmail",
+  }
+];
+
+module.exports = { questions };
